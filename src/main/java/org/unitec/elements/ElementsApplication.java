7186d46dcac91f9e2e59a0b9d27edaeca5f1766e
brand <brand@10.20.0.191>
2019-01-25 21:33:52 -0600

    Spring Project push

---
+++ src/main/java/org/unitec/elements/ElementsApplication.java
@@ -0,0 +1,14 @@
+package org.unitec.elements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElementsApplication.class, args);
+	}
+
+}
+
