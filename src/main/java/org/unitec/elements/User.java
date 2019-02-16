0b15a5a58d18d32cba307df7071c0e7bda7d4af5
brand <brand@10.20.0.191>
2019-02-15 21:44:22 -0600

    s

---
+++ src/main/java/org/unitec/elements/User.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.elements;
+
+/**
+ *
+ * @author brand
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private Localizacion localizacion;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Localizacion getLocalizacion() {
+        return localizacion;
+    }
+
+    public void setLocalizacion(Localizacion localizacion) {
+        this.localizacion = localizacion;
+    }
+}
